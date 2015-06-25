58ec41ad1153a1a28b2ac054da653828054da779
Colm O'Sullivan <cbpos1989@gmail.com>
2015-06-25 13:26:34 +0100

    Testing egit on new computer

---
+++ CourseWork/src/com/github/cbpos1989/example/HelloWorld2.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.github.cbpos1989.example;
+
+/**
+ * @author User1
+ *
+ */
+public class HelloWorld2 {
+	public static void main(String[] arg){
+		System.out.println("Hello World");
+	}
+}

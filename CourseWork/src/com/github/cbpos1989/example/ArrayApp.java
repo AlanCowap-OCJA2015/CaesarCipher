5dc2bec7af0b4bb0f89b9c0b05779c921c2065b6
Colm O'Sullivan <cbpos1989@gmail.com>
2015-07-01 10:47:58 +0100

    ArrayApp now uses Random numbers to populate the scores array

--- CourseWork/src/com/github/cbpos1989/example/ArrayApp.java
+++ CourseWork/src/com/github/cbpos1989/example/ArrayApp.java
@@ -3,6 +3,7 @@
  */
 package com.github.cbpos1989.example;
 
+import java.util.Random;
 /**
  * App to demonstrate how to initialise and populate an array.
  * 
@@ -12,21 +13,25 @@ package com.github.cbpos1989.example;
 import java.util.Scanner;
 
 public class ArrayApp{
-	int arraySize = 100;
-	int scoresTotal = 0;
-	int minValue = 0;
-	int maxValue = 0;
+	private int arraySize = 100;
+	private int scoresTotal = 0;
+	private int minValue = 0;
+	private int maxValue = 0;
 	private Scanner scan;
+	
 
 	public static void main(String[] args){
 		//System.out.println("Working");
 		ArrayApp myArrayApp = new ArrayApp();
 		myArrayApp.inputData();
 		myArrayApp.creatNewArray(myArrayApp.arraySize);
-		
-	
 	}
 
+	private int randomGenerator(){
+		Random rand = new Random();
+		return rand.nextInt(101);
+	}
+	
 	void inputData(){
 		scan = new Scanner(System.in);
 		System.out.print("Enter size of scores array: ");
@@ -41,7 +46,7 @@ public class ArrayApp{
 		
 		for (int i = 0; i < scores.length; ++i) {
 			
-			scores[i] = (i+1);
+			scores[i] = (randomGenerator());
 			System.out.print(scores[i] + " ");
 
 			scoresTotal = scores[i] + scoresTotal;
@@ -69,7 +74,7 @@ public class ArrayApp{
 		double meanNumber = (double)scoresTotal/scores.length;
 		
 		
-		System.out.printf("\nMean number of array: " + meanNumber);
+		System.out.println("\nMean number of array: " + meanNumber);
 		System.out.println("Smallest number in array: " + minNumber);
 		System.out.println("Largest number in array: " + maxNumber);
 		System.out.println("Total number: " + scoresTotal);

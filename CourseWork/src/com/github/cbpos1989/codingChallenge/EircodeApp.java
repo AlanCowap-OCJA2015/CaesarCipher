bae5c366e596738977562c05296411b26e07e579
Craig McGarry <craigmcgarry@upcmail.ie>
2015-07-14 12:06:06 +0100

    new options in menu added

--- CourseWork/src/com/github/cbpos1989/codingChallenge/EircodeApp.java
+++ CourseWork/src/com/github/cbpos1989/codingChallenge/EircodeApp.java
@@ -12,33 +12,46 @@ import java.util.Scanner;
 public class EircodeApp {
 	String eircodes[] = {"D02 Y006","D04 C932", "D15 XR2R", "D03 RR27", "D24 H510","D02 XE81","DO2 P656"};
 	String address[] = {"5 Merrion Square North, Dublin 2","10 Burlington Road, Dublin 4","Dunsink Observatory,"
-						+ " Dunsink Lane, Dublin 15","26 Kincora Road, Clontarf, Dublin 3.",
-						"Partas, 4A Brookfield Enterprise Centre, Dublin 24","Hodges Figgis , 56-58 Dawson Street , Dublin 2"
-						,"Central Bank of Ireland , Dame Street , Dublin 2"};
-	
+			+ " Dunsink Lane, Dublin 15","26 Kincora Road, Clontarf, Dublin 3.",
+			"Partas, 4A Brookfield Enterprise Centre, Dublin 24","Hodges Figgis , 56-58 Dawson Street , Dublin 2"
+			,"Central Bank of Ireland , Dame Street , Dublin 2"};
+
 	public static void main(String[] args) {
 		EircodeApp ea = new EircodeApp();
 		ea.initialiseApp();
 
 	}
-	
+
 	void initialiseApp(){
 		boolean invalidChoice = true;
 		Scanner scan = new Scanner(System.in);
 		do{
 
 			String oldChoice ;
-			System.out.println("---Eircode\u00A9 App Menu---\n1) Enter eircode\n2) Quit");
+			System.out.println("---Eircode\u00A9 App Menu---\n1) Enter eircode\n2) Enter address\n3) Enter postcode"
+					+ "\n4) Enter custom eircode and address\n5) Quit");
 			try {
 				oldChoice = scan.nextLine();
 				int choice = Integer.parseInt(oldChoice);
 				if(choice==1){
 					System.out.print("Please enter a valid Eircode: ");
 					String temp = scan.nextLine();
-					
 					System.out.print("Eircode Address: " + checkForAddress(temp.toUpperCase()));
 					invalidChoice = false;
 				}else if(choice==2){
+					System.out.print("Please enter a valid address : ");
+					String temp = scan.nextLine();
+					System.out.print("Eircode : " + checkForAddress(temp.toUpperCase()));
+					invalidChoice = false;
+				}else if(choice==3){
+					System.out.print("Please enter a valid postcode : ");
+					String postCode = scan.nextLine();
+					invalidChoice = false;
+				}else if(choice==4){
+					System.out.print("Please enter a valid address and eir code : ");
+					invalidChoice = false;
+				}else if(choice ==5){
+					//quit
 					System.out.println("Thank You for using the Eircode\u00A9 app");
 					invalidChoice = false;
 				}else{
@@ -57,7 +70,7 @@ public class EircodeApp {
 		scan.close();
 
 	}
-	
+
 	String checkForAddress(String eircode){
 		String eircodeNoSpace = eircode.replaceAll("[^A-Za-z0-9]", "");
 		String eircodesNoSpace = null;

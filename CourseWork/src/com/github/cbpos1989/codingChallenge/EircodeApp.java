1f172e56913e860a06673c0e7f0fc77a1f2f19aa
Craig McGarry <craigmcgarry@upcmail.ie>
2015-07-14 13:39:49 +0100

    updated array to accept u.i

--- CourseWork/src/com/github/cbpos1989/codingChallenge/EircodeApp.java
+++ CourseWork/src/com/github/cbpos1989/codingChallenge/EircodeApp.java
@@ -10,16 +10,18 @@ import java.util.Scanner;
  *
  */
 public class EircodeApp {
-	String eircodes[] = {"D02 Y006","D04 C932", "D15 XR2R", "D03 RR27", "D24 H510","D02 XE81","DO2 P656"};
+	String eircodes[] = {"D02 Y006","D04 C932", "D15 XR2R", "D03 RR27", "D24 H510","D02 XE81","DO2 P656","null","null","null"};
 	String address[] = {"5 Merrion Square North, Dublin 2","10 Burlington Road, Dublin 4","Dunsink Observatory,"
 			+ " Dunsink Lane, Dublin 15","26 Kincora Road, Clontarf, Dublin 3.",
 			"Partas, 4A Brookfield Enterprise Centre, Dublin 24","Hodges Figgis , 56-58 Dawson Street , Dublin 2"
-			,"Central Bank of Ireland , Dame Street , Dublin 2"};
+			,"Central Bank of Ireland , Dame Street , Dublin 2","null","null","null"};
 
 	public static void main(String[] args) {
 		EircodeApp ea = new EircodeApp();
 		ea.initialiseApp();
-
+		String userAddress1 = "";
+		String userAddress2 = "";
+		String userAddress3 = "";
 	}
 
 	void initialiseApp(){
@@ -52,8 +54,18 @@ public class EircodeApp {
 						break;
 				case 4:	System.out.println("How many addresses/eir codes do you want to add ");
 						String uiOldChoice = scan.nextLine();
-						int uiChoice = Integer.parseInt(uiOldChoice); break;
+						int uiChoice = Integer.parseInt(uiOldChoice); 
+						for(int i = 8;i<(8+uiChoice);++i){
+							System.out.println("Enter address please : ");
+							address[i]=scan.nextLine();
+							System.out.println("Enter eir code please : ");
+							eircodes[i]=scan.nextLine();
+							
+						}
+						
+						break;
 				case 5: System.out.println("Thank You for using the Eircode\u00A9 app");
+						
 						invalidChoice = false;
 						break;
 				default:System.out.println("Please enter a valid option from the menu!!");

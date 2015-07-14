8a68011040ce2c27b8ce482aeef97588523db814
Colm O'Sullivan <cbpos1989@gmail.com>
2015-07-14 09:14:19 +0100

    Initial commmit of EircodeApp

---
+++ CourseWork/src/com/github/cbpos1989/codingChallenge/EircodeApp.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.github.cbpos1989.codingChallenge;
+
+/**
+ * @author User1
+ *
+ */
+public class EircodeApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		EircodeApp ea = new EircodeApp();
+		ea.initialiseApp();
+
+	}
+	
+	void initialiseApp(){
+		System.out.print("Working");
+	}
+
+}

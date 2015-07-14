438d5d0eb35b6f3ab9913c505bba1c3f35a3e47a
Marie Archbold <Mariearchbold1@gmail.com>
2015-07-14 09:37:18 +0100

    Added menu

--- CourseWork/src/com/github/cbpos1989/codingChallenge/EircodeApp.java
+++ CourseWork/src/com/github/cbpos1989/codingChallenge/EircodeApp.java
@@ -3,6 +3,8 @@
  */
 package com.github.cbpos1989.codingChallenge;
 
+import java.util.Scanner;
+
 /**
  * @author User1
  *
@@ -19,7 +21,23 @@ public class EircodeApp {
 	}
 	
 	void initialiseApp(){
-		System.out.print("Working");
+		//System.out.print("Working")
+		boolean invalidChoice = true;
+		do{
+		Scanner scan = new Scanner(System.in);
+		String oldChoice ;
+		System.out.println("---Eircode App Menu---\n1) Enter eircode\n2) Quit");
+		oldChoice = scan.next();
+		int choice = Integer.parseInt(oldChoice);
+		if(choice==1){
+			
+			invalidChoice = false;
+		}else{
+			System.out.println("Thank You for using the eircode app\u00A9");
+			invalidChoice = false;
+		}
+		
+	}while(invalidChoice);
+		
 	}
-
 }

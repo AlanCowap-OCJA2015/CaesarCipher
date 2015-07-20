80aea79182692dd2eb1e15f667a5d7b2fbb87707
User1 <User1@User2-PC>
2015-07-20 12:25:51 +0100

    new

--- CourseWork/src/com/teamccs/ceasercipher/CeaserCipherApp.java
+++ CourseWork/src/com/teamccs/ceasercipher/CeaserCipherApp.java
@@ -1,20 +1,46 @@
-/**
- * 
- */
 package com.teamccs.ceasercipher;
+
+import java.util.Scanner;
 
 /**
  * @author User1
  *
  */
 public class CeaserCipherApp {
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public static void main(String[] args) {		
+		
+		
+		CeaserCipherApp cC = new CeaserCipherApp();
+		cC.userInput();
+			
+	}
+	
+	public void userInput(){
+		System.out.println("Please input your text:");
+		Scanner scan = new Scanner(System.in);
+		do{
+		String input = scan.nextLine();
+//		System.out.println(stringToCharConvert(scan.nextLine()));
+		}while(;);
+	}
+	
+	public String stringToCharConvert(String input){
+		char[] convertedString = input.toCharArray();	
+		char[] tempCharArray = new char[convertedString.length];
+		String output = "";
+		for(int i = 0; i < convertedString.length; ++i){
+			tempCharArray[i] = shiftChar(1, convertedString[i]);
+			output += tempCharArray[i];
+		}
+		return output;
+	}
+	
+	private char shiftChar(int shift, char c){
+		
+		c = (char) (c+(shift));
+		
+		return c;
 	}
 
 }

c28b47c0fdf6d99132be12dc483d44b64e1d5ffd
Artjoms Porss <artjoms.porss@gmail.com>
2015-07-20 12:20:09 +0100

    added comments

--- EncryptionApp.java
+++ EncryptionApp.java
@@ -13,18 +13,21 @@ public class EncryptionApp {
 	public static void main(String[] args) {
 
 		int shiftCounter = 1;
-
 		Scanner scan = new Scanner(System.in);
+		
+		//get user input
 		System.out.print("Enter text to be encrypted: " );
 		String userInput = scan.nextLine();
+		//store user input in char[]
 		char[] inputChars = userInput.toCharArray();
 
+		//iterate throught char[]
 		for(int i = 0; i < inputChars.length; ++i){
-			if(inputChars[i] >= 32 && inputChars[i] <= 126){
-				if(inputChars[i] == 126){
+			if(inputChars[i] >= 32 && inputChars[i] <= 126){	//if char is within boundaries
+				if(inputChars[i] == 126){	//make last char to be first char within boundaries
 					inputChars[i] = 32;
-				}else{
-					inputChars[i] = (char) (inputChars[i] + shiftCounter);
+				}else{						//if it's not last char within bounds, increment it
+					inputChars[i] = (char) (inputChars[i] + shiftCounter);	
 				}
 			}
 		}

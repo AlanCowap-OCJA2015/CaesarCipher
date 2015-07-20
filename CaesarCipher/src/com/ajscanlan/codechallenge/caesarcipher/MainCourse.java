db2024853bd024d78c1c4479344c98b1af3ad179
Alex Scanlan <ajscanlan27@gmail.com>
2015-07-20 12:10:23 +0100

    Added Menu Option

--- CaesarCipher/src/com/ajscanlan/codechallenge/caesarcipher/MainCourse.java
+++ CaesarCipher/src/com/ajscanlan/codechallenge/caesarcipher/MainCourse.java
@@ -8,16 +8,32 @@ public class MainCourse {
 	
 	public static void main(String[] args) {
 		String userInput;
-		System.out.println("Welcome to Our Salad Security");
+		MainCourse main = new MainCourse();
+		main.displayDish();
+		
+		/*System.out.println("Welcome to Our Salad Security");
 		System.out.println("Please Enter a word to Encrypt");
 		Scanner scan = new Scanner(System.in);
 		userInput = scan.nextLine();
 		
 		CaeserSalad salad = new CaeserSalad();
 		System.out.print(salad.encryptLettuce(userInput));
-		
+		*/
 		//salad.dencryptLettuce("Mfuuvdf");
 		//System.out.println(salad.dencryptLettuce("Mfuuvdf"));
+	}
+	
+	private void displayDish(){
+		System.out.println("Welcome To Salad Security");
+		System.out.println("");
+		System.out.println("1.) Encryption");
+		System.out.println("2.) Decryption");
+		System.out.println("3.) Quit Progam");
+		System.out.println("");
+		System.out.println("Please Enter Options 1 , 2 or 3");
+		
+		Scanner scan = new Scanner(System.in);
+		
 	}
 
 }

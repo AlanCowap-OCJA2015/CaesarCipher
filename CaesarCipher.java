7e5d93324ca87ea15b0c4ed08eedd94479298707
JennQuinn42 <jennquinn42@gmail.com>
2015-07-20 13:21:14 +0100

    Update CaesarCipher.java
    Updated to include user specified shift value and direction

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -12,32 +12,52 @@ public class CeasarCipher {
 	public static void main(String[] args) {
 
 		String userText;
+		int userDirection;
 		String encrypt;
 		String decryption;
+		int shiftValue;
+		boolean direction = true;
 
 		Scanner scan = new Scanner (System.in);
 		System.out.println("please enter text to encrypt:");
 		userText = scan.nextLine();
 
-		encrypt = encrypt(userText);
+		System.out.println("please enter the shift value:");
+		shiftValue = scan.nextInt();
+
+		System.out.println("please enter if you want to shift right(1) or left(2):");
+		userDirection = scan.nextInt();
+
+		if(userDirection == 1){
+			direction = true;
+		}
+		else if(userDirection == 2){
+			direction = false;
+		}
+		else{
+
+		}
+
+		encrypt = encrypt(userText, shiftValue, direction);
 		System.out.println("Encrypt...");
 		System.out.println(encrypt);
 
 		System.out.println("Press enter to decrypt");
 		try{
 			System.in.read();
-			
+
 		}catch(Exception e){
-			
+
 		}
-		
+
 		decryption = decrypt(encrypt);
 		System.out.println("Decrypt...");
 		System.out.println(decryption);
 	}
 
 	//Method to encrypt a given string
-	private static String encrypt(String plainText){
+	private static String encrypt(String plainText, int shiftValue, 
+			boolean direction){
 		String encrypt = "";
 		for(int i = 0 ; i < plainText.length(); ++i){
 			char c = plainText.charAt(i);
@@ -45,9 +65,16 @@ public class CeasarCipher {
 				encrypt += c;
 				continue;
 			}
-			++c;
+			if(direction){
+				c += shiftValue;
+			}
+			else{
+				c-= shiftValue;
+			}
 			if((int)c > 126){
-				c = 32;
+				c -= 95;
+			}else if((int)c < 32){
+				c += 95;
 			}
 			encrypt += c;
 		}
@@ -55,7 +82,8 @@ public class CeasarCipher {
 	}//end of encrypt
 
 	//Method to decrypt a given String
-	private static String decrypt(String encryption){
+	private static String decrypt(String encryption, int shiftValue, 
+			boolean direction){
 		String decryption = "";
 		for(int i = 0; i < encryption.length(); ++i){
 			char c = encryption.charAt(i);
@@ -63,9 +91,16 @@ public class CeasarCipher {
 				decryption += c;
 				continue;
 			}
-			--c;
-			if((int)c < 32){
-				c = 126;
+			if(direction){
+				c -= shiftValue;
+			}
+			else{
+				c += shiftValue;
+			}
+			if((int)c > 126){
+				c -= 95;
+			}else if((int)c < 32){
+				c += 95;
 			}
 			decryption += c;
 		}

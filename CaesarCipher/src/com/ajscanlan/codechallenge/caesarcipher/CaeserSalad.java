81b2f410fe892eb8738ac205548e22b875f91b67
Alex Scanlan <ajscanlan27@gmail.com>
2015-07-20 14:45:48 +0100

    hail the spud lord!!!

--- CaesarCipher/src/com/ajscanlan/codechallenge/caesarcipher/CaeserSalad.java
+++ CaesarCipher/src/com/ajscanlan/codechallenge/caesarcipher/CaeserSalad.java
@@ -4,39 +4,48 @@ import java.util.Scanner;
 
 public class CaeserSalad {
 
-	String encryptLettuce(int shiftySalad){
-		Scanner scan = new Scanner(System.in);
-		System.out.println("please enter the sentence you want to encypt");
+	void encryptLettuce(int choice){
+
+
+		System.out.println("Please enter shift magnitude");
+
+		Scanner scan = new Scanner(System.in);		
+		int shiftySalad = scan.nextInt();
+
+		if(choice == 2){
+			shiftySalad = -shiftySalad;
+		}
+
+
+		scan = new Scanner(System.in);
+
+		System.out.println("please enter the sentence you want to encypt/decrypt");
 		String text = scan.nextLine();
 		char tempTomato;
 		char [] charText = text.toCharArray();
-		
+
 		for(int i = 0 ; i < charText.length;i++ ){
 			tempTomato = charText[i];
 			tempTomato += shiftySalad;
+			if(tempTomato > 126){
+				//tempTomato = 32;
+				int temp = tempTomato - 126;
+				temp = 32 + temp;
+				tempTomato = (char)temp;
+
+				
+			}else if( tempTomato < 32){
+				int temp = tempTomato + 126;
+				temp = temp - 32;
+				tempTomato = (char)temp;
+			}
 			charText[i] = tempTomato;
 		}
 		String temp = new String(charText);
-	//	scan.close();
-		return temp;
+		//	scan.close();
+		System.out.println(temp);
 	}
-	
-	String dencryptLettuce(int shiftySalad){
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Please enter the sentence you want to decrypt");
-		String text = scan.nextLine();
-		char tempTomato;
-		char [] charText = text.toCharArray();
-		
-		for(int i = 0 ; i < charText.length;i++ ){
-			tempTomato = charText[i];
-			tempTomato -= shiftySalad;
-			charText[i] = tempTomato;
-		}
-		String temp = new String(charText);
-	//	scan.close();
-		return temp;
-	}
-	
-	
+
+
+
 }

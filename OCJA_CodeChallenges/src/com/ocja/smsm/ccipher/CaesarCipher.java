dba44f1763fecfb855504bd7bc5e880c82622841
Sam Downes <sammdownes@gmail.com>
2015-07-20 13:50:47 +0100

    Added shift direction and magnitude

--- OCJA_CodeChallenges/src/com/ocja/smsm/ccipher/CaesarCipher.java
+++ OCJA_CodeChallenges/src/com/ocja/smsm/ccipher/CaesarCipher.java
@@ -45,13 +45,15 @@ public class CaesarCipher {
 		String decryptText;
 		CaesarCipher cc = new CaesarCipher();
 		int offset = 1;
+		int direction = 1;
 		char[] userTextArray = null;
 		boolean isRunning = true;
 		do{
 			System.out.println("1) Enter a String ");
-			System.out.println("2) Encrypt a string ");
-			System.out.println("3) Decrypt a string");
-			System.out.println("4) Quit ");
+			System.out.println("2) Change the encryption magnitude and direction");
+			System.out.println("3) Encrypt the stored string ");
+			System.out.println("4) Decrypt the stored string");
+			System.out.println("5) Quit ");
 
 			Scanner scan = new Scanner(System.in);
 			int userChoice = scan.nextInt();
@@ -63,17 +65,25 @@ public class CaesarCipher {
 				System.out.println(userText);
 				userTextArray = userText.toCharArray();
 				break;
-
-			case 2 : 
-				userTextArray = cc.encrypt(userTextArray, offset);
-				System.out.println(String.valueOf(userTextArray));
+			case 2 :
+				System.out.println("Please enter L for left or R for right");
+				String tempString = scan.next();
+				if (tempString.toUpperCase().startsWith("L")){
+					direction = -1;
+				}
+				System.out.println("Please choose the magnitude to shift the string (between 0 and 26)");
+				offset = scan.nextInt();
 				break;
-
 			case 3 : 
-				userTextArray = cc.decrypt(userTextArray, offset);
+				userTextArray = cc.encrypt(userTextArray, offset*direction);
 				System.out.println(String.valueOf(userTextArray));
 				break;
-			case 4:
+
+			case 4 : 
+				userTextArray = cc.decrypt(userTextArray, offset*direction);
+				System.out.println(String.valueOf(userTextArray));
+				break;
+			case 5:
 				System.out.println("Thank you, goodbye!");
 				isRunning = false;
 				break;

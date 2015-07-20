2f424658aaba25fa8367bd110132bbb3cf0d382a
Colm O'Sullivan <cbpos1989@gmail.com>
2015-07-20 15:27:14 +0100

    working on menu

--- CourseWork/src/com/teamccs/ceasercipher/CeaserCipherApp.java
+++ CourseWork/src/com/teamccs/ceasercipher/CeaserCipherApp.java
@@ -11,21 +11,27 @@ public class CeaserCipherApp {
 	private int shiftDirection = 1;
 	boolean isEncypting = true;
 	private String userText;
-	
+
 	public static void main(String[] args) {		
 		CeaserCipherApp cC = new CeaserCipherApp();
 		cC.userMenu();
-			
+
 	}
-	
+
 	private void userMenu(){
 		boolean isRunning = true;
 		Scanner scan = new Scanner(System.in);
 		int inputNum = 0;
-			
 		do{
 			inputNum = 0;
-			System.out.println("Ceaser Cipher Menu\n1)Enter Input\n2)Shift Direction\n3)Shift Magnitude\n4)Encryption/Decryption\n5)Print Output\n6)Exit");
+			System.out.println("\nCeaser Cipher Menu");
+			System.out.println("1)Enter Input  (" + userText + ")" );
+			//System.out.println("2)Shift Direction (" + getDirection() + ")");
+			System.out.println("2)Shift Direction");
+			System.out.println("3)Shift Magnitude");
+			System.out.println("4)Encryption/Decryption");
+			System.out.println("5)Print Output");
+			System.out.println("6)Exit");
 			System.out.print("Enter Choice: ");
 			String input = scan.nextLine();
 			try{
@@ -34,7 +40,7 @@ public class CeaserCipherApp {
 				System.out.println("Invalid option, please pick a valid number");
 				isRunning = true;
 			};
-			
+
 			switch(inputNum){
 			case 1:userInput(); break;
 			case 2:userDirectionInput(); break;
@@ -42,23 +48,33 @@ public class CeaserCipherApp {
 			case 4:setEncryption();break;
 			case 5:printOutput();break;
 			case 6:isRunning = false;break;
-				default:break;
-		};
+			default:break;
+			};
 		} while(isRunning);
-		
-		
-	
+
+	}
+
+	private void printOutput() {
+		if(userText != null){
+			System.out.println("Encrypted Text: " + stringToCharConvert(userText));		
+		}else{
+			System.out.println("No input given");
+		}
 	}
 	
-	private void printOutput() {
-		System.out.println("Encrypted Text: " + stringToCharConvert(userText));		
+	private String getDirection(){
+		if (shiftDirection > 0){
+			return "right";
+		} else {
+			return "left";
+		}
 	}
 
 	private void userDirectionInput(){
 		boolean invalidInput = true;
 		Scanner scan = new Scanner(System.in);
 		do{
-			System.out.println("Please choose shift Direction (R = right, L = left");
+			System.out.println("Please choose shift Direction (R = right, L = left)");
 			String input = scan.next();
 			if(input.equalsIgnoreCase("R")){
 				setDirection(true);
@@ -70,9 +86,9 @@ public class CeaserCipherApp {
 				invalidInput = true;
 			}
 		} while(invalidInput);
-	
+
 	}
-	
+
 	private void userMagnitudeInput(){
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Please select the magnitude value of encryption: ");
@@ -81,15 +97,15 @@ public class CeaserCipherApp {
 		this.shiftMagnitude *= this.shiftDirection;
 	}
 
-	
+
 	private void setDirection(boolean right){
 		if(!right){
 			this.shiftDirection *= -1;
 		}
 	}
-	
-	
-	
+
+
+
 	private void setEncryption(){
 		boolean invalidInput = true;
 		Scanner scan = new Scanner(System.in);
@@ -107,17 +123,17 @@ public class CeaserCipherApp {
 				invalidInput = true;
 			}
 		} while(invalidInput);
-	
+
 	}
-	
+
 	public void userInput(){
 		boolean invalidInput = true;
-		
+
 		Scanner scan = new Scanner(System.in);
 		do{
 			System.out.println("Please input your text:");
 			userText = scan.nextLine();
-			
+
 			if(userText.isEmpty()){
 				System.out.println("Please enter valid input");
 				invalidInput = true;
@@ -127,7 +143,7 @@ public class CeaserCipherApp {
 
 		}while(invalidInput);
 	}
-	
+
 	public String stringToCharConvert(String input){
 		char[] convertedString = input.toCharArray();	
 		char[] tempCharArray = new char[convertedString.length];
@@ -138,7 +154,7 @@ public class CeaserCipherApp {
 		}
 		return output;
 	}
-	
+
 	private char shiftChar(int shift, char c){
 		if(isEncypting){
 			if(c == 126){
@@ -161,4 +177,3 @@ public class CeaserCipherApp {
 	}
 
 }
-	

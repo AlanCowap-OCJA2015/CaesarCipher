d72cfff7380f93b9ffd202aeefa247e3646737fb
Alex Scanlan <ajscanlan27@gmail.com>
2015-07-20 12:03:29 +0100

    Encryption Commit

--- CaesarCipher/src/com/ajscanlan/codechallenge/caesarcipher/CaeserSalad.java
+++ CaesarCipher/src/com/ajscanlan/codechallenge/caesarcipher/CaeserSalad.java
@@ -2,4 +2,31 @@ package com.ajscanlan.codechallenge.caesarcipher;
 
 public class CaeserSalad {
 
+	String encryptLettuce(String text){
+		char tempTomatoe;
+		char [] charText = text.toCharArray();
+		
+		for(int i = 0 ; i < charText.length;i++ ){
+			tempTomatoe = charText[i];
+			tempTomatoe++;
+			charText[i] = tempTomatoe;
+		}
+		String temp = new String(charText);
+		return temp;
+	}
+	
+	String dencryptLettuce(String text){
+		char tempTomatoe;
+		char [] charText = text.toCharArray();
+		
+		for(int i = 0 ; i < charText.length;i++ ){
+			tempTomatoe = charText[i];
+			tempTomatoe--;
+			charText[i] = tempTomatoe;
+		}
+		String temp = new String(charText);
+		return temp;
+	}
+	
+	
 }
--- CaesarCipher/src/com/ajscanlan/codechallenge/caesarcipher/MainCourse.java
+++ CaesarCipher/src/com/ajscanlan/codechallenge/caesarcipher/MainCourse.java
@@ -1,9 +1,23 @@
 package com.ajscanlan.codechallenge.caesarcipher;
 
+import java.util.Scanner;
+
 public class MainCourse {
 
+	
+	
 	public static void main(String[] args) {
-		System.out.println("hllo world");
+		String userInput;
+		System.out.println("Welcome to Our Salad Security");
+		System.out.println("Please Enter a word to Encrypt");
+		Scanner scan = new Scanner(System.in);
+		userInput = scan.nextLine();
+		
+		CaeserSalad salad = new CaeserSalad();
+		System.out.print(salad.encryptLettuce(userInput));
+		
+		//salad.dencryptLettuce("Mfuuvdf");
+		//System.out.println(salad.dencryptLettuce("Mfuuvdf"));
 	}
 
 }
